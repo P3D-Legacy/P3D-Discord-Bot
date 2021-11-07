0583995120c3ed80828b909ab1a12bf99f71f32b
Aristeidis <aristeidis.karavas@ifabrik.de>
2021-11-07 22:00:28 +0100

    [feature] Create categories and channels on Bot join

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
 import discord
 from discord.ext import commands
+from pprint import pprint
 import websockets as create_connection
 import modules.utility.general as general
 
 client = commands.Bot(command_prefix="!")
+
+
+@client.event
+async def on_guild_join(guild):
+    pd3_category = discord.utils.get(guild.categories, id=general.get_category_id())
+    pd3_server_chat = discord.utils.get(guild.channels, id=general.get_server_chat_channel_id())
+
+    if not pd3_category:
+        pd3_category = await guild.create_category(general.get_pd3_category_name(), overwrites=None, reason=None)
+
+    if not pd3_server_chat:
+        await guild.create_text_channel(general.get_pd3_server_chat_name(), category=pd3_category)
+
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
 
 
 
--- modules/utility/general.py
+++ modules/utility/general.py
@@ -15,3 +15,19 @@ def log(s):
 def get_api_uri():
     return "wss://karp.pokemon3d.net/next/api/v1/communication/listener/ws"
 
+
+def get_category_id():
+    return
+
+
+def get_server_chat_channel_id():
+    return
+
+
+def get_pd3_category_name():
+    return "Server API"
+
+
+def get_pd3_server_chat_name():
+    return "PD3 Server chat"
+
